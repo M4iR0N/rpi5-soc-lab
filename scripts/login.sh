3e2bf4b69e0e1c3052f3bce08e73cd826fb223d5
M4iR0N <26681130+M4iR0N@users.noreply.github.com>
2025-12-28 18:37:00 +0000

    updating login script, placeholder SSH keygen and SSH server login

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,1 +1,16 @@
-placeholder file ~ update soon
+#!/bin/bash
+
+echo "Do you wish to create an SSH key? Yes or No?"
+read user_input
+
+if [ "$user_input" == "yes" ]; then
+
+	echo "SSH key created"
+else
+	echo "You have chosen not to create an SSH key"
+	echo "Please enter SSH server IP here:"
+	read server_ip
+	
+	ssh $server_ip
+
+fi
